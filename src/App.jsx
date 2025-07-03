84063fb8119bd770fb3ccfc175177332eb08dfeb
trancyenma <148397318+trancyenma@users.noreply.github.com>
2025-07-02 23:22:17 -0400

    1:32:28

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,82 @@
+import styled, { ThemeProvider } from "styled-components";
+import { AuthContextProvider } from "./context/AuthContext";
+import { MyRoutes } from "./routers/routers";
+import { createContext, useState } from "react";
+import { Light, Dark } from "./styles/themes";
+import { Device } from "./styles/breackpoints";
+import { Sidebar } from "./components/organismos/sidebard/Sidebard";
+export const ThemeContext = createContext(null);
 
-import styled from 'styled-components';
-import { AuthContextProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import { MyRoutes } from './routers/routers';
 
 function App() {
+  const [themeuse, setTheme] = useState("dark");
+  const theme = themeuse === "light" ? Light : Dark ;
+  const themeStyle = theme ;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-   <>
-   <AuthContextProvider>  
-    <Container>
-     <MyRoutes/>
-    </Container>  
-      
-   </AuthContextProvider>
-   </>
-  )
+    <>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            <Container className={sidebarOpen?'active':''}>
+              <section className="ContentSidebard">
+                  SIDEBAR
+              </section>
+              <section className="ContentMenuambur">
+                Menu
+              </section>
+              <section className="ContentRoutes">
+                <MyRoutes />
+              </section>
+              
+            </Container>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </>
+  );
 }
 
-const Container  =styled.div`
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${({theme}) => theme.bgtotal};
 
-`
+  .ContentSidebar{
+  display: none;
+  }
+  .ContentMenuambur{
+  display:block;
+  position:absolute;
+  left:20px;
+  }
 
-export default App
+  @media ${Device.tablet}{
+  grid-template-columns: 65px 1fr;
+
+  &.active{
+   grid-template-columns:220px 1fr;
+
+  }
+  .ContentSidebar{
+  display: initial;
+  }
+  .ContentMenuambur{
+  display:none;
+ 
+  }
+
+  .ContentRoutes{
+    grid-column:1;
+    width:100%;
+    @media ${Device.tablet}{
+    grid-column:2;           
+    }
+    
+  }
+
+
+  }
+`;
+
+export default App;
--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import styled from 'styled-components';
 
 function HomeTemplate() {
   return (
-    <div>HomeTemplate XD</div>
+    <Container>
+      <h1>Componente Home</h1>
+    </Container>
+    
   )
 }
-
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items:center;
+height:100vh;
+overflow: hidden;
+background-color: ${({theme}) => theme.bgtotal};
+color: ${({theme})=>theme.text};
+width:100%;
+`;
 export default HomeTemplate
